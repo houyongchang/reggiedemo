e42dc18e87c363b69e0f08afe810cb3c515f5501
hyc <2560728442@qq.com>
2023-06-08 16:38:29 +0800

    项目分支提交2次

--- src/main/java/com/itcode/ReggieApplication.java
+++ src/main/java/com/itcode/ReggieApplication.java
@@ -17,6 +17,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class ReggieApplication {
     public static void main(String[] args) {
         log.info("启动成功");
+        log.info("6666");
         SpringApplication.run(ReggieApplication.class, args);
     }
 
